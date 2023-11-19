36bac1c580b66fbeeb5499b65272a89ac2077952
xshady <fastik036@gmail.com>
2023-11-19 16:19:24 +0300

    simplify build helper

--- build_helper/src/lib.rs
+++ build_helper/src/lib.rs
@@ -11,8 +11,7 @@ const AUTO_GENERATED: &str = "
 ";
 
 #[doc(hidden)]
-pub fn __generate_bindings(get_code: impl FnOnce() -> TokenStream, side: &str, out_file: &str) {
-    let code = get_code();
+pub fn __generate_bindings(code: TokenStream, side: &str, out_file: &str) {
     let side: syn::Ident = syn::parse_str(side).unwrap();
 
     let code = quote! {
@@ -20,20 +19,20 @@ pub fn __generate_bindings(get_code: impl FnOnce() -> TokenStream, side: &str, o
         pub use #side::*;
     };
 
-    let formatted_host_bindings =
+    let formatted_bindings =
         prettyplease::unparse(&syn::parse_file(&code.to_string()).unwrap_or_else(|e| {
             panic!("prettyplease failed with error: {e}, code: {code}");
         }));
 
     let path = format!("src/{out_file}");
-    fs::write(path, format!("{AUTO_GENERATED}{formatted_host_bindings}")).unwrap();
+    fs::write(path, format!("{AUTO_GENERATED}{formatted_bindings}")).unwrap();
 }
 
 #[macro_export]
 macro_rules! generate_bindings {
     ($side:ident, $interface_file:literal, $out_file:literal) => {
         $crate::__generate_bindings(
-            || $crate::wasm_codegen_impl::$side($interface_file),
+            $crate::wasm_codegen_impl::$side($interface_file),
             stringify!($side),
             $out_file,
         );
